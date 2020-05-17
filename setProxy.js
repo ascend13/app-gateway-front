fac42fea5a2ff267e7996f6c23e7d71cba5e4333
WSH <384035991@qq.com>
2020-05-17 22:55:57 +0800

    mobx

--- setProxy.js
+++ setProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(proxy("/", {
+    app.use(proxy("/ ", {
         target: "http://150.158.113.204:8000/api/v1/gin-user-center",
         // target: "http://localhost:9080",
         changeOrigin: true,
