9a09a745798271d3868b49310d42f02b060d998e
WSH <384035991@qq.com>
2020-05-19 00:32:46 +0800

    请求封装

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,1 +1,46 @@
 import axios from "axios";
+import { Spin, message } from 'antd';
+
+export default {
+    async get(url:string, param?: any, isLoding = false) {
+        try {
+            const res = await axios({
+                method: 'get',
+                url: url,
+                // headers: {
+                //     "Token": getCookie("Token") || '',
+                //     "Token-Id": getCookie("User-Id") || '',
+                // }
+                data: {},
+                params: param
+            })
+            return new Promise((resolve, reject) => {
+                if(res.data.code === 0) resolve(res.data)
+                else reject(res)
+            })
+        }
+        catch(err) {
+            message.error(err)
+        }
+    },
+    async post(url:string, param?: any, isLoding = false) {
+        try {
+            const res = await axios({
+                method: 'post',
+                url: url,
+                // headers: {
+                //     "Token": getCookie("Token") || '',
+                //     "Token-Id": getCookie("User-Id") || '',
+                // }
+                data: param,
+            })
+            return new Promise((resolve, reject) => {
+                if(res.data.code === 0) resolve(res.data)
+                else reject(res)
+            })
+        }
+        catch(err) {
+            message.error(err)
+        }
+    }
+}
