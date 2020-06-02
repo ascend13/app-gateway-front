7b784cf7115b2f5697fe2068a5a2249e5447a9f2
WSH <384035991@qq.com>
2020-06-02 23:04:57 +0800

    使用Normalize.css进行样式初始化

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '@/assets/css/base.scss';
+import 'normalize.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
